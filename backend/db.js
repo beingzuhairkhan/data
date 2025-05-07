92127d5c13ad6dbae2687393aca4c1a1cdfad754
zuhair khan <zuhairkhan5134@gmail.com>
2025-05-07 23:07:24 +0530

    done

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,11 @@
 
 import mongoose from 'mongoose';
+
+const MONGO_URI = "mongodb+srv://zuhairk7890o:2p5g8sdu6O5DdOjj@cluster0.2aj5kpy.mongodb.net/"
+
+
 const connectDB = async () => {
-    try { await mongoose.connect(process.env.MONGO_URI); console.log('MongoDB connected'); }
+    try { await mongoose.connect(MONGO_URI); console.log('MongoDB connected'); }
     catch (error) { console.error('Database connection failed', error); process.exit(1); }
 };
 export default connectDB;
